bacd20b3896a672a8d4c39403521e52c4d26ec5e
Raffi Hotter <rhotter@users.noreply.github.com>
2019-02-25 22:19:34 -0500

    can now write data to file

---
+++ offline/training_software_testing/scripts/stream_data_wifi.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+import sys
+
+sys.path.append('..')  # help python find cyton.py relative to scripts folder
+from openbci import wifi as bci
+import logging
+
+
+def printData(sample):
+    print(sample.sample_number)
+    print(sample.channel_data)
+
+
+if __name__ == '__main__':
+    shield_name = 'OpenBCI-E2B6'
+    logging.basicConfig(filename="test.log", format='%(asctime)s - %(levelname)s : %(message)s', level=logging.DEBUG)
+    logging.info('---------LOG START-------------')
+    shield = bci.OpenBCIWiFi(shield_name=shield_name, log=True, high_speed=False)
+    print("WiFi Shield Instantiated")
+    shield.start_streaming(printData)
+
+    shield.loop()
---
+++ offline/training_software_testing/scripts/stream_data_wifi_high_speed.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+import sys
+
+sys.path.append('..')  # help python find cyton.py relative to scripts folder
+from openbci import wifi as bci
+import logging
+
+
+def printData(sample):
+    print(sample.sample_number)
+    print(sample.channel_data)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(filename="test.log", format='%(asctime)s - %(levelname)s : %(message)s', level=logging.DEBUG)
+    logging.info('---------LOG START-------------')
+    # If you don't know your IP Address, you can use shield name option
+    # If you know IP, such as with wifi direct 192.168.4.1, then use ip_address='192.168.4.1'
+    shield_name = 'OpenBCI-E218'
+    shield = bci.OpenBCIWiFi(shield_name=shield_name, log=True, high_speed=True)
+    print("WiFi Shield Instantiated")
+    shield.start_streaming(printData)
+
+    shield.loop()
