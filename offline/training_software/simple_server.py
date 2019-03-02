1fa8e689333f37b598f7b8ecdc58e9633bebb88a
Raffi Hotter <rhotter@users.noreply.github.com>
2019-03-02 13:15:13 -0500

    added buttons for left/right/rest/wait

--- offline/training_software/simple_server.py
+++ offline/training_software/simple_server.py
@@ -11,26 +11,28 @@ import csv
 import datetime
 import time
 
-import matplotlib.pyplot as plt
-
 def write_time_data(*args):
     test_number = args[1][0]
     time_data = list(args[2:])
-    plt.plot([1,2,3],[1,2,3])
-    plt.show()
     time = datetime.datetime.now()
-    with open('../data/sample-tests/time_test{}.csv'.format(test_number), 'a') as f:
-        writer = csv.writer(f, delimiter=',')
-        writer.writerow([time] + time_data)
+    with open('settings.txt', 'r') as f:
+        settings = f.read()
+    if settings!= 'wait':
+        with open('../data/sample-tests/time_test{}.csv'.format(test_number), 'a') as f:
+            writer = csv.writer(f, delimiter=',')
+            writer.writerow([settings] + [time] + time_data)
 
 def write_fft_data(*args):
     test_number = args[1][0]
     channel = int(args[2])
     frequency_data = list(args[3:])
     time = datetime.datetime.now()
-    with open('../data/sample-tests/fft_test{}_channel{}.csv'.format(test_number, channel), 'a') as f:
-        writer = csv.writer(f, delimiter=',')
-        writer.writerow([time] + frequency_data)
+    with open('settings.txt', 'r') as f:
+        settings = f.read()
+    if settings!= 'wait':
+        with open('../data/sample-tests/fft_test{}_channel{}.csv'.format(test_number, channel), 'a') as f:
+            writer = csv.writer(f, delimiter=',')
+            writer.writerow([settings] + [time] + frequency_data)
 
 dispatcher = dispatcher.Dispatcher()
 
