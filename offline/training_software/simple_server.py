64fdd2212d1a5a4d12ede04831542ab0b8c2bd6d
Raffi Hotter <rhotter@users.noreply.github.com>
2019-03-03 18:41:26 -0500

    python server

--- offline/training_software/simple_server.py
+++ offline/training_software/simple_server.py
@@ -12,83 +12,59 @@ import datetime
 import time
 
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-if __name__ == '__main__':
-    socketio.run(app)
+emit('Channel1', {'time': 42, 'y': 42390})
 
-# def write_time_data(*args):
-#     test_number = args[1][0]
-#     time_data = list(args[2:])
-#     time = datetime.datetime.now()
-#     with open('settings.txt', 'r') as f:
-#         settings = f.read()
-#     print(settings)
-#     if settings!= 'wait':
-#         with open('../data/sample-tests/time_test{}.csv'.format(test_number), 'a') as f:
-#             writer = csv.writer(f, delimiter=',')
-#             writer.writerow([settings] + [time] + time_data)
-#
-# def write_fft_data(*args):
-#     test_number = args[1][0]
-#     channel = int(args[2])
-#     frequency_data = list(args[3:])
-#     time = datetime.datetime.now()
-#     with open('settings.txt', 'r') as f:
-#         settings = f.read()
-#     print(settings)
-#     if settings!= 'wait':
-#         with open('../data/sample-tests/fft_test{}_channel{}.csv'.format(test_number, channel), 'a') as f:
-#             writer = csv.writer(f, delimiter=',')
-#             writer.writerow([settings] + [time] + frequency_data)
+def write_time_data(*args):
+    test_number = args[1][0]
+    time_data = list(args[2:])
+    time = datetime.datetime.now()
+    with open('settings.txt', 'r') as f:
+        settings = f.read()
+    print(settings)
+    if settings!= 'wait':
+        with open('../data/sample-tests/time_test{}.csv'.format(test_number), 'a') as f:
+            writer = csv.writer(f, delimiter=',')
+            writer.writerow([settings] + [time] + time_data)
 
-# if __name__ == '__main__':
-    # eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+def write_fft_data(*args):
+    test_number = args[1][0]
+    channel = int(args[2])
+    frequency_data = list(args[3:])
+    time = datetime.datetime.now()
+    with open('settings.txt', 'r') as f:
+        settings = f.read()
+    print(settings)
+    if settings!= 'wait':
+        with open('../data/sample-tests/fft_test{}_channel{}.csv'.format(test_number, channel), 'a') as f:
+            writer = csv.writer(f, delimiter=',')
+            writer.writerow([settings] + [time] + frequency_data)
 
-#
-#
-# def write_time_data(*args):
-#     test_number = args[1][0]
-#     time_data = list(args[2:])
-#     time = datetime.datetime.now()
-#     with open('settings.txt', 'r') as f:
-#         settings = f.read()
-#     print(settings)
-#     if settings!= 'wait':
-#         with open('../data/sample-tests/time_test{}.csv'.format(test_number), 'a') as f:
-#             writer = csv.writer(f, delimiter=',')
-#             writer.writerow([settings] + [time] + time_data)
-#
-# def write_fft_data(*args):
-#     test_number = args[1][0]
-#     channel = int(args[2])
-#     frequency_data = list(args[3:])
-#     time = datetime.datetime.now()
-#     with open('settings.txt', 'r') as f:
-#         settings = f.read()
-#     print(settings)
-#     if settings!= 'wait':
-#         with open('../data/sample-tests/fft_test{}_channel{}.csv'.format(test_number, channel), 'a') as f:
-#             writer = csv.writer(f, delimiter=',')
-#             writer.writerow([settings] + [time] + frequency_data)
-#
-# dispatcher = dispatcher.Dispatcher()
-#
+dispatcher = dispatcher.Dispatcher()
+
 # def get_data(test_number=0):
-#     """
-#     Main function in program
-#     """
-#     global dispatcher
-#     dispatcher.map("/openbci-fft", write_fft_data, test_number)
-#     dispatcher.map("/openbci-time", write_time_data, test_number)
-#
-#     ip = "127.0.0.1"
-#     port = 12345
-#
-#     server = osc_server.ThreadingOSCUDPServer((ip, port), dispatcher)
-#     print("Serving on {}".format(server.server_address))
-#     server.serve_forever()
+"""
+Main function in program
+"""
+
+if __name__ == '__main__':
+    test_number=0
+    # global dispatcher
+    dispatcher.map("/openbci-fft", write_fft_data, test_number)
+    dispatcher.map("/openbci-time", write_time_data, test_number)
+
+    ip = "127.0.0.1"
+    port = 12345
+
+    server = osc_server.ThreadingOSCUDPServer((ip, port), dispatcher)
+    print("Serving on {}".format(server.server_address))
+    socketio.run(app)
+    server.serve_forever()
+
+
+
 # get_data()
--- src/dunno_what_im_doing/app.py
+++ src/dunno_what_im_doing/app.py
@@ -15,13 +15,10 @@ def index():
                 f.write('right')
                 f.close()
         elif request.form['submit_button'] == 'rest':
-<<<<<<< HEAD
             with open("../../offline/training_software/settings.txt", 'w') as f:
                 f.write('rest')
                 f.close()
         elif request.form['submit_button'] == 'wait':
-=======
->>>>>>> 49ef48035381c48124343e94981ace4560a274a6
             with open("../../offline/training_software/settings.txt", 'w') as f:
                 f.write('rest')
                 f.close()
