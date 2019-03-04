84db24935ba45867ef4d4998b23f1edaf909e352
Marley <marleyxiong0@gmail.com>
2019-03-03 23:14:13 -0500

    added averaging for spectrogram

--- offline/physionet_epoched_spectrogram.py
+++ offline/physionet_epoched_spectrogram.py
@@ -69,7 +69,7 @@ def plot_specgram(spec_freqs, spec_PSDperBin,title,i=1):
     f_lim_Hz = [0, 20]   # frequency limits for plotting
     #plt.figure(figsize=(10,5))
     plt.title(title)
-    spec_t = [i*.1 for i in range(len(all_specgram[0]))]
+    spec_t = [i*.1 for i in range(len(spec_PSDperBin[0]))]
     plt.subplot(3,1,i)
     plt.pcolor(spec_t, spec_freqs, spec_PSDperBin, cmap='rainbow')  # dB re: 1 uV
     #plt.clim([-25,26])
@@ -101,8 +101,16 @@ def plot_data(raw, channel_name):
     draw_specgram(left, 160, fig, 2, channel_name + " left")
     draw_specgram(right, 160, fig, 3, channel_name + " right")
     plt.subplots_adjust(hspace=0.5)
+def av_specgram(epochs, event_name, channel_name, i=1):
+    channel_data = epochs[event_name]._data[:,raw.ch_names.index(channel_name),:]
+    all_specgram = []
+    for sess in channel_data:
+        t, f, d = get_specgram(sess, 160)
+        all_specgram.append(d)
+    av = np.mean(np.array(all_specgram), axis=0)
+    plot_specgram(f, av,channel_name,i=i)
 subject = 1
-runs = [4,8,12] 
+runs = [3,7,11] 
 channel_name = 'C4'
 raw_fnames = eegbci.load_data(subject, runs)
 raw_files = [read_raw_edf(f, preload=True, stim_channel='auto') for f in
@@ -132,34 +140,22 @@ picks = pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False,
 # Testing will be done with a running classifier
 epochs = Epochs(raw, events, event_id, tmin, tmax, proj=True, picks=picks,
                 baseline=None, preload=True)
-plt.figure()
+
 
 # check epochs['right'].data.shape
-channel_data = epochs['right']._data[:,raw.ch_names.index(channel_name),:]
-_,_,all_specgram = get_specgram(channel_data[0],160)
-for sess in channel_data[1:]:
-    t, f, d = get_specgram(sess, 160)
-    all_specgram = np.hstack((all_specgram, d))
-plot_specgram(f, all_specgram,channel_name)
 
-channel_data = epochs['left']._data[:,raw.ch_names.index(channel_name),:]
-_,_,all_specgram = get_specgram(channel_data[0],160)
-for sess in channel_data[1:]:
-    t, f, d = get_specgram(sess, 160)
-    all_specgram = np.hstack((all_specgram, d))
-plot_specgram(f, all_specgram,channel_name, i=2)
+plt.figure()
+av_specgram(epochs, 'right', channel_name)
+av_specgram(epochs, 'left', channel_name, i=2)
+av_specgram(epochs, 'rest', channel_name, i=3)
 
-channel_data = epochs['rest']._data[:,raw.ch_names.index(channel_name),:]
-_,_,all_specgram = get_specgram(channel_data[0],160)
-for sess in channel_data[1:]:
-    t, f, d = get_specgram(sess, 160)
-    all_specgram = np.hstack((all_specgram, d))
-plot_specgram(f, all_specgram,channel_name, i=3)
-
-
-
-
+plt.figure()
 channel_name = 'C3'
+av_specgram(epochs, 'right', channel_name)
+av_specgram(epochs, 'left', channel_name, i=2)
+av_specgram(epochs, 'rest', channel_name, i=3)
+
+
 plt.figure()
 
 # check epochs['right'].data.shape
