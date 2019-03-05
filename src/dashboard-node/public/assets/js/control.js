73c5ab3004d9dd85853fc9d6402e8554aa84d150
Raffi Hotter <rhotter@users.noreply.github.com>
2019-03-04 20:01:24 -0500

    real time stuff

--- src/dashboard-node/public/assets/js/control.js
+++ src/dashboard-node/public/assets/js/control.js
@@ -34,52 +34,52 @@ $(document).ready(function() {
 
 
   //Sample Data and Graphs Please Ignore this block:
-  var data = [
-    { label: 'Layer 1', values: [ {x: 0, y: 0}, {x: 1, y: 1}, {x: 2, y: 2} ] },
-    { label: 'Layer 2', values: [ {x: 0, y: 0}, {x: 1, y: 1}, {x: 2, y: 4} ] }
-  ];
-  var sinLayer = {label: 'sin', values: []},
-    cosLayer = {label: 'cos', values: []}
-
-    for (var x = 0; x <= 2*Math.PI; x += Math.PI / 64) {
-      sinLayer.values.push({ x: x, y: Math.sin(x) + 1 });
-      cosLayer.values.push({ x: x, y: Math.cos(x) + 1 });
-    }
-  var sensorChart2 = $('#sensor2').epoch({
-      type: 'area',
-      data: [sinLayer, cosLayer],
-      axes: ['left', 'right', 'bottom']
-  });
-  var sensorChart3 = $('#sensor3').epoch({
-      type: 'area',
-      data: [sinLayer, cosLayer],
-      axes: ['left', 'right', 'bottom']
-  });
-  var sensorChart4 = $('#sensor4').epoch({
-      type: 'area',
-      data: [sinLayer, cosLayer],
-      axes: ['left', 'right', 'bottom']
-  });
-  var sensorChart5 = $('#sensor5').epoch({
-      type: 'area',
-      data: [sinLayer, cosLayer],
-      axes: ['left', 'right', 'bottom']
-  });
-  var sensorChart6 = $('#sensor6').epoch({
-      type: 'area',
-      data: [sinLayer, cosLayer],
-      axes: ['left', 'right', 'bottom']
-  });
-  var sensorChart7 = $('#sensor7').epoch({
-      type: 'area',
-      data: [sinLayer, cosLayer],
-      axes: ['left', 'right', 'bottom']
-  });
-  var sensorChart8 = $('#sensor8').epoch({
-      type: 'area',
-      data: [sinLayer, cosLayer],
-      axes: ['left', 'right', 'bottom']
-  });
+  // var data = [
+  //   { label: 'Layer 1', values: [ {x: 0, y: 0}, {x: 1, y: 1}, {x: 2, y: 2} ] },
+  //   { label: 'Layer 2', values: [ {x: 0, y: 0}, {x: 1, y: 1}, {x: 2, y: 4} ] }
+  // ];
+  // var sinLayer = {label: 'sin', values: []},
+  //   cosLayer = {label: 'cos', values: []}
+  //
+  //   for (var x = 0; x <= 2*Math.PI; x += Math.PI / 64) {
+  //     sinLayer.values.push({ x: x, y: Math.sin(x) + 1 });
+  //     cosLayer.values.push({ x: x, y: Math.cos(x) + 1 });
+  //   }
+  // var sensorChart2 = $('#sensor2').epoch({
+  //     type: 'area',
+  //     data: [sinLayer, cosLayer],
+  //     axes: ['left', 'right', 'bottom']
+  // });
+  // var sensorChart3 = $('#sensor3').epoch({
+  //     type: 'area',
+  //     data: [sinLayer, cosLayer],
+  //     axes: ['left', 'right', 'bottom']
+  // });
+  // var sensorChart4 = $('#sensor4').epoch({
+  //     type: 'area',
+  //     data: [sinLayer, cosLayer],
+  //     axes: ['left', 'right', 'bottom']
+  // });
+  // var sensorChart5 = $('#sensor5').epoch({
+  //     type: 'area',
+  //     data: [sinLayer, cosLayer],
+  //     axes: ['left', 'right', 'bottom']
+  // });
+  // var sensorChart6 = $('#sensor6').epoch({
+  //     type: 'area',
+  //     data: [sinLayer, cosLayer],
+  //     axes: ['left', 'right', 'bottom']
+  // });
+  // var sensorChart7 = $('#sensor7').epoch({
+  //     type: 'area',
+  //     data: [sinLayer, cosLayer],
+  //     axes: ['left', 'right', 'bottom']
+  // });
+  // var sensorChart8 = $('#sensor8').epoch({
+  //     type: 'area',
+  //     data: [sinLayer, cosLayer],
+  //     axes: ['left', 'right', 'bottom']
+  // });
 
 
   //Depending on range, changes the countdown text and vice versa
