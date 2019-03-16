3570c709dd67c58c49dd1259c7d4fd0627bc144c
opent03 <baviet.nguyen@mail.mcgill.ca>
2019-03-16 09:00:08 -0400

    Adaboost

--- offline/ML/sqggly_clf.py
+++ offline/ML/sqggly_clf.py
@@ -4,6 +4,7 @@ Attempt to classify the squiggly data that was provided by Santiago
 '''
 import numpy as np
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
@@ -18,9 +19,9 @@ data = np.load(loc + 'feature_matrix')
 data = np.array(data)
 assert type(data) == np.ndarray
 
-#X = np.log(data[:,:-1]) # normalization, kind of
-X = data[:,-3:-1]/10000
-#X = normalize(X, axis=0)
+X = np.log(data[:,:-1]) # normalization, kind of
+#X = data[:,-3:-1]/10000
+X = normalize(X, axis=0)
 y = np.array(data[:, -1], dtype=np.uint8)
 
 '''
@@ -38,15 +39,15 @@ print(X)
 plt.scatter(X[:,0], X[:,1], marker='o', c=y, cmap='viridis', alpha=0.3)
 plt.xlabel('per_win_max')
 plt.ylabel('per_win_mean')
-plt.show()
-exit()
+#plt.show()
+
 '''
 ML
 '''
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, test_size=0.2)
 
 
-clf = SVC(C=1)
+clf = AdaBoostClassifier(n_estimators=2000)
 
 clf.fit(X_train, y_train)
 pred = clf.predict(X_test)
