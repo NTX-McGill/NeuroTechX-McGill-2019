e2de0ff1ba80699ad318ae4510b6aaef48fecee2
Viet Nguyen <viet@vmbp.local>
2019-03-16 18:47:22 -0400

    Added stacking stuff. Almost working

--- offline/ML/sqggly_clf.py
+++ offline/ML/sqggly_clf.py
@@ -10,8 +10,10 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
 from sklearn.decomposition import PCA
+from sklearn.model_selection import KFold, StratifiedKFold
 from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.svm import SVC
 from keras.models import Sequential
@@ -32,6 +34,31 @@ def train_clf(clf, name, X_train, X_test, y_train, y_test):
     pred = clf.predict(X_test)
     scr = accuracy_score(y_test, pred)
     print('{} accuracy score: {:.4f}'.format(name, scr))
+
+def construct_metafeatures(clf_arr, X_train, X_test, y_train):
+    'This method is a load of crap and shouldnt be used ever'
+    train, test = list(), list()
+    for clf in clf_arr:
+        print(clf)
+        clf.fit(X_train, y_train)
+        train_pred = clf.predict(X_train)
+        test_pred = clf.predict(X_test)
+        train.append(train_pred)
+        test.append(test_pred)
+    return np.array(train).T, np.array(test).T
+
+def Stacking(model,train,y,test,n_fold):
+    folds = StratifiedKFold(n_splits=n_fold,random_state=1)
+    test_pred = []
+    train_pred = []
+    for train_indices, val_indices in folds.split(train,y):
+        x_train, x_val = train[train_indices], train[val_indices]
+        y_train, y_val = y[train_indices], y[val_indices]
+
+        model.fit(X=x_train,y=y_train)
+        train_pred.append(model.predict(x_val))
+        test_pred.append(model.predict(test))
+    return np.array(train_pred).T, np.array(test_pred).T
 
 loc = '../signal_processing/'
 
@@ -112,13 +139,41 @@ model.compile(loss='binary_crossentropy', optimizer='nadam', metrics=['accuracy'
 model.fit(X_train, y_train)
 '''
 
-#clf = KNeighborsClassifier(n_neighbors=7)
-clf = XGBClassifier(n_estimators=1000, learning_rate=0.1, max_depth=3,
+clf1 = MLPClassifier(hidden_layer_sizes=(70,), solver='adam', learning_rate='adaptive', max_iter=2000)
+clf2 = XGBClassifier(n_estimators=1000, learning_rate=0.1, max_depth=3,
                     objective='binary:logistic',gamma=7)
-#clf = SVC(C=100)
-#clf = RandomForestClassifier(n_estimators=500)
-#clf = ExtraTreesClassifier(n_estimators=1000)
-#clf = MLPClassifier(hidden_layer_sizes=(90,60,), solver='sgd', learning_rate='adaptive',
-#                        learning_rate_init=0.035, max_iter=2000, momentum=0.87)
+clf3 = SVC(C=100)
+clf4 = RandomForestClassifier(n_estimators=1000)
+clf5 = ExtraTreesClassifier(n_estimators=1000)
+clf6 = MLPClassifier(hidden_layer_sizes=(90,60,), solver='sgd', learning_rate='adaptive',
+                        learning_rate_init=0.035, max_iter=2000, momentum=0.87)
+clf7 = LogisticRegression(solver='saga')
+clf8 = KNeighborsClassifier(n_neighbors=3)
+clf9 = KNeighborsClassifier(n_neighbors=5)
+clfA = KNeighborsClassifier(n_neighbors=7)
+clfB = KNeighborsClassifier(n_neighbors=9)
+clfC = GaussianNB()
+clfD = LinearDiscriminantAnalysis()
+clfE = AdaBoostClassifier(n_estimators=500)
+clfF = XGBClassifier(n_estimators=500, objective='binary:logistic', gamma=7)
 
-train_clf(clf, 'xgb', X_train, X_test, y_train, y_test)
+'''
+new_X_train, new_X_test = construct_metafeatures(
+    [clf1, clf2, clf3, clf4, clf5, clf6, clf7, clf8, clf9,
+    clfA, clfB, clfC, clfD, clfE], 
+    X_train, X_test, y_train)
+'''
+
+n_fold = 10
+train1, test1 = Stacking(clf1, X_train, y_train, X_test, n_fold)
+#train2, test2 = Stacking(clf2, X_train, y_train, X_test, n_fold)
+
+print(train1.shape, test1.shape)
+exit()
+new_X_train = np.concatenate((train1, train2), axis=1)
+new_X_test = np.concatenate((test1, test2), axis=1)
+
+clfF.fit(new_X_train, y_train)
+pred = clfF.predict(new_X_test)
+scr = accuracy_score(y_test, pred)
+print('Meta classifier score: {:.4f}'.format(scr))
