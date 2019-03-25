97bde841c8c828f48b78df3fba1d865b4b56f19b
Kenji Marshall <kenji.marshall99@gmail.com>
2019-03-24 22:09:25 -0400

    frequency display and loop

--- src/dashboard/public/assets/js/control.js
+++ src/dashboard/public/assets/js/control.js
@@ -11,4 +11,18 @@ $(document).ready(function() {
       $('#startProduction').html("Start &nbsp; <i class='fas fa-play fa-sm text-white'></i>");
     }
   });
+
+  socket.on('sample rate', function(data){
+      
+      if(data['sample rate'] > 250*1.15 || data['sample rate'] < 250*0.85 ){
+          $('#sampleRate').css('background-color', 'red');
+          $('#sampleRate').css('border-color', 'red');
+      }
+      else{
+          $('#sampleRate').css('background-color', 'grey');
+          $('#sampleRate').css('border-color', 'grey');
+      }
+      $('#sampleRate').html(data['sample rate'] + " hz");
+      console.log('SAMPLE RATE RECEIVED OF : ' + data['sample rate']);
+    });
 });
