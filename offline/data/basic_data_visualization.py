0de7a2c23b1f0d294407eeb363094651e06a68b1
Marley <marleyxiong0@gmail.com>
2019-03-29 23:56:22 -0400

    average spec

--- offline/data/basic_data_visualization.py
+++ offline/data/basic_data_visualization.py
@@ -24,19 +24,21 @@ def get_spectral_content(ch, fs_Hz, shift=0.1):
     spec_PSDperBin = spec_PSDperHz * fs_Hz / float(NFFT)
     return spec_t, spec_freqs, spec_PSDperBin  # dB re: 1 uV
 
-def draw_specgram(spec_freqs, spec_PSDperBin, fig, num_subplots, i, title):
+def draw_specgram(spec_freqs, spec_PSDperBin, fig, num_subplots, i, title=None):
     spec_t = [idx*.1 for idx in range(len(spec_PSDperBin[0]))]
     f_lim_Hz = [0, 20]   # frequency limits for plotting
     #plt.figure(figsize=(10,5))
     plt.subplot(num_subplots,2,i)
-    #plt.title(title)
+    if title:
+        plt.title(title)
     plt.pcolormesh(spec_t, spec_freqs, 10*np.log10(spec_PSDperBin))  # dB re: 1 uV
-    plt.clim([-25,20])
+    plt.clim([-25,25])
     plt.xlim(spec_t[0], spec_t[-1]+1)
     plt.ylim(f_lim_Hz)
     # plt.xlabel('Time (sec)') aint got space for this
     plt.ylabel('Frequency (Hz)')
-    #plt.subplots_adjust(hspace=1)
+    if title:
+        plt.subplots_adjust(hspace=0.6)
     #plt.gca().set_aspect('equal', adjustable='box')
 
 def filter_(arr, fs_Hz, lowcut, highcut, order):
@@ -45,8 +47,15 @@ def filter_(arr, fs_Hz, lowcut, highcut, order):
    for i in range(0, order):
        arr = signal.lfilter(b, a, arr, axis=0)
    return arr
+def resize_max(specgram, fillval=np.nan):
+    max_length = max([len(el[0]) for el in specgram])
+    return np.array([pad_block(el, max_length,fillval) for el in specgram])
+def pad_block(block, max_length, fillval):
+    padding = np.full([len(block), max_length-(len(block[0]))], fillval)
+    return np.hstack((block,padding))
 def get_data(fname, csv):
     df = pd.read_csv(csv)
+    channel = (1,2,3,4,5,6,7,8,13)
     data = np.loadtxt(fname,
                   delimiter=',',
                   skiprows=7,
@@ -68,46 +77,40 @@ def get_data(fname, csv):
             prev = idx
             prev_direction = el
     return all_data
-filenames = sorted([f for f in glob.glob("*.txt")])
 sampling_freq = 250
 shift = 0.1
-channel = (1,2,3,4,5,6,7,8,13)
 fs_Hz = 250
+folder = "March22_008/"
+#folder = "March20/"
 #fname = 'time-test-JingMingImagined10s-2019-3-20-10-12-1.csv'
 csv = "time-test-JingMingImaginedREALLYGOOD-2019-3-20-10-21-44.csv"
 csv = "time-test-JingMingActual_10s-2019-3-20-10-51-28.csv"
 csv = "time-test-JingMingActual-2019-3-20-9-54-59.csv"
 csv = "time-test-JingMingActual-2019-3-20-10-5-15.csv"
-folder = "March22_008/"
-csv = "10_008-2019-3-22-15-8-55.csv"
-#csv = "9_008-2019-3-22-14-59-0.csv"
-#csv = "8_008-2019-3-22-14-45-53.csv"
-csv = "7_008-2019-3-22-14-27-46.csv"
-csv = "6_008-2019-3-22-14-19-52.csv"
-csv = "5_008-2019-3-22-14-10-26.csv"
+csv = "time-test-JingMingImagined_10s-2019-3-20-10-28-35.csv"
+#csv = "time-test-JingMingImagined_10s-2019-3-20-10-30-26.csv"
+#csv = "time-test-JingMingImagined_10s-2019-3-20-10-35-31.csv"
+#csv = "time-test-JingMingImagined_10s-2019-3-20-10-57-45.csv"
 
 fname = 'OpenBCI-RAW-2019-03-20_10-04-29.txt'
-fname = "10_008_OpenBCI-RAW-2019-03-22_15-07-58.txt"
-#fname = "8to9_008_OpenBCI-RAW-2019-03-22_13-49-24.txt"
-fname = "4to7_008_OpenBCI-RAW-2019-03-22_13-49-24.txt"
 
-#indices = np.where(np.logical_and(timestamps>=start, timestamps<=end))
-#eeg = eeg[indices]
+if folder == "March22_008/":
+    csv = "10_008-2019-3-22-15-8-55.csv"
+    csv = "9_008-2019-3-22-14-59-0.csv"
+    csv = "8_008-2019-3-22-14-45-53.csv"
+    #csv = "7_008-2019-3-22-14-27-46.csv"
+    #csv = "6_008-2019-3-22-14-19-52.csv"
+    #csv = "5_008-2019-3-22-14-10-26.csv"
+    
+    fname = "10_008_OpenBCI-RAW-2019-03-22_15-07-58.txt"
+    fname = "8to9_008_OpenBCI-RAW-2019-03-22_13-49-24.txt"
+    #fname = "4to7_008_OpenBCI-RAW-2019-03-22_13-49-24.txt"
 
-
-#a = df.loc[df['Direction'] == 'Right'][['Channel 1','Channel 2']]
-'''
-a = df[['Channel 1','Channel 2']]
-data = np.copy(a.values)
-data = filter_(data, sampling_freq,1,40,1)
-print(data.shape)
-fig = plt.figure(figsize=(20,10))
-length = 60
-data.resize((int(len(data)/(250 * length)) + 1, 250 * length,data.shape[-1]))'''
 all_data = get_data(folder + fname, folder + csv)
 
-set_1 = [0,1,2,3]
-set_2 = [4,5,6,7]
+set_1 = [0,1]
+set_2 = [6,7]
+num = 1
 for direction, data in all_data.items():
     figname = direction + '_spec_' + csv.split('.')[0]
     fig = plt.figure(figname)
@@ -126,12 +129,18 @@ for direction, data in all_data.items():
     num_rows = max(len(left), 5)
     for spec in left:
         idx += 1
-        draw_specgram(f, spec, fig, num_rows, idx * 2 - 1, fname)
+        draw_specgram(f, spec, fig, num_rows, idx * 2 - 1)
     idx = 0
     for spec in right:
         idx += 1
-        draw_specgram(f, spec, fig, num_rows, idx * 2, fname)
+        draw_specgram(f, spec, fig, num_rows, idx * 2)
     plt.savefig(figname)
+    
+    fig = plt.figure('av_spec_' + csv.split('.')[0])
+    draw_specgram(f, np.mean(resize_max(left), axis=0), fig, 3,num,direction + ", channel 1-4")
+    print(num)
+    draw_specgram(f, np.mean(resize_max(right), axis=0), fig, 3,num+1,direction + ", channel 5-8")
+    num += 2
     
     figname = direction + '_mu_' + csv.split('.')[0]
     fig = plt.figure(figname)
@@ -141,13 +150,14 @@ for direction, data in all_data.items():
         idx += 1
         plt.subplot(num_rows,1,idx)
         mu = np.mean(spec[mu_indices], axis=0)
-        plt.plot(mu)
+        plt.plot(mu, label="0 set")
     idx = 0
     for spec in right:
         idx += 1
         plt.subplot(num_rows,1,idx)
         mu = np.mean(spec[mu_indices], axis=0)
-        plt.plot(mu)
+        plt.plot(mu, label="8 set")
+    plt.legend()
 '''
 start = df['Time'][0]
 end = df['Time'].iloc[-1]
