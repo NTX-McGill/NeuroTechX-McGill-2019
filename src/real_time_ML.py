b2df81954985f66956a1e755946f2e545d500a51
Marley <marleyxiong0@gmail.com>
2019-03-31 02:30:57 -0400

    all the logic for wheelchair decisions

--- src/real_time_ML.py
+++ src/real_time_ML.py
@@ -13,70 +13,155 @@ with open('../offline/model.pkl', 'rb') as fid:
     clf = pickle.load(fid)
 print("STARTING")
 
-state = "stop"
+wheelchair_state = "stop"
 
-def filter_(raw_eeg_data,bp_lowcut =1, bp_highcut =60, bp_order=5,
-            notch_freq_Hz  = [60, 120], notch_order =5):
-   nyq = 0.5 * 250 #Nyquist frequency
-   low = bp_lowcut / nyq
-   high = bp_highcut / nyq
+# configs
+pred_frequency = 0.2
+distance_from_artifact_s = 2    # number of seconds away from artifact we require to consider a window a blink
+blink_threshold_s = 1           # number of seconds of blink state required before we send 'BLINK', also the number of seconds of blink state before we send 'BLINK' again in intermediate state **must tune
+change_threshold_s = 2          # number of seconds after a recent state change we require before changing the state again
+decision_s = 3                  # number of seconds in intermediate before we force a decision
 
-   #Butter
-   b_bandpass, a_bandpass = butter(bp_order, [low , high], btype='band', analog=True)
+# thresholds
+distance_from_artifact = int(distance_from_artifact_s/pred_frequency)
+blink_threshold = int(blink_threshold_s/pred_frequency)
+change_threshold = int(change_threshold_s/pred_frequency)
+decision = int(decision_s/pred_frequency)
+clear_threshold = int(1/pred_frequency)                                 # the minimum number of windows within the decision span that must be clear to be considered a 'turn' signal
 
-   bp_filtered_eeg_data = np.apply_along_axis(lambda l: lfilter(b_bandpass, a_bandpass ,l),0,raw_eeg_data)
+# counters
+last_artifact = 0
+num_blinks = 0
+last_change = 0
 
-   notch_filtered_eeg_data = bp_filtered_eeg_data
+# buffers
+prev_states = []
+prev_predictions = []
 
-   low1  = notch_freq_Hz[0]
-   high1 = notch_freq_Hz[1]
-   low1  = low1/nyq
-   high1 = high1/nyq
-
-   b_notch, a_notch = iirfilter(2, [low1, high1], btype='bandstop')
-   notch_filtered_eeg_data = np.apply_along_axis(lambda l: lfilter(b_notch, a_notch ,l),
-                                                     0,bp_filtered_eeg_data)
-
-
-   return np.array(notch_filtered_eeg_data)
 def predict(ch):
     """
     BASELINE PREDICTION ALGORITHM FOR MVP
     ch has shape (2, 500)
     """
-    threshold = 1
-    #ch = np.array(ch)
-
-    #ch = filter_(ch)
+    global last_artifact
+    global num_blinks
+    global last_change
+    global prev_predictions
+    global prev_states
+    
+    last_change = min(100, last_change + 1) # increment the distance (in num predictions) away from the last state change
+    # the min function is there in case there's overflow
     ch = np.array(ch).T
-
-    psd1,freqs = mlab.psd(np.squeeze(ch[0]),
-                           NFFT=500,
-                           Fs=250)
-    mu_indices = np.where(np.logical_and(freqs>=10, freqs<=12))
-    mu1 = np.mean(psd1[mu_indices])
-
-    psd2,freqs = mlab.psd(np.squeeze(ch[7]),
-                           NFFT=500,
-                           Fs=250)
-    mu2 = np.mean(psd2[mu_indices])
-    blink = psd2[70:100].mean() + psd1[70:100].mean()
-    print(blink)
-    result = list(clf.predict_proba(np.array([mu1,mu2]).reshape(1,-1))[0])
-    print(result)
-    if result[0] > 0.6:
-        return "L"
-    elif result[1] > 0.6:
-        return "R"
+    psd1, freqs = mlab.psd(np.squeeze(ch[0]),
+                                   NFFT=500,
+                                   window=mlab.window_hanning,
+                                   Fs=250,
+                                   noverlap=0
+                                   ) 
+    psd2, freqs = mlab.psd(np.squeeze(ch[7]),
+                                   NFFT=500,
+                                   window=mlab.window_hanning,
+                                   Fs=250,
+                                   noverlap=0
+                                   ) 
+    blink_index = np.where(np.logical_and(freqs>= 5, freqs <= 6))
+    blink_psd = psd1[blink_index].mean() + psd2[blink_index].mean()
+    indices = np.where(np.logical_and(freqs>=15, freqs<=45))
+    high_psd = psd1[indices].mean() + psd2[indices].mean()
+    
+    """ compute the brain state and stateful counters here """
+    if (high_psd > 30):
+        last_artifact = 0   # the most recent artifact was 0 seconds ago
+        state = 'artifact'
     else:
-        return "F"
+        last_artifact = min(100, last_artifact + 1)          # increment distance from artifact
+        if (blink_psd > 20):
+            if last_artifact >= distance_from_artifact:
+                state = 'blink'
+            else:
+                state = 'fake_blink'
+        else:
+            state = 'clear'
+            
+    if (state == 'blink'):  # if we have a blink
+        num_blinks += 1
+    else:
+        num_blinks = 0      # reset num consecutive blinks to 0
+    
+    print(state, wheelchair_state)
+    
+    """ now use the brain state and wheelchair state to make decisions """
+    if wheelchair_state == "stop" or wheelchair_state == "forward":
+        if last_change > change_threshold:      # we don't want the wheelchair to switch back immediately if there's residual blinks
+            if num_blinks >= blink_threshold:
+                print('BLINK')
+                num_blinks = 0
+                return 'BLINK'
+    
+    if wheelchair_state == "intermediate":
+        prev_states.append(state)
+        if prev_states.count('blink') >= blink_threshold:
+            prev_predictions = []
+            prev_states = []
+            return 'BLINK'
+        if last_change > decision:              # we've reached decision deadline
+            if prev_states.count('clear') > clear_threshold:    # enough samples are clear, we interpret the user's intention as wanting to turn
+                mu_indices = np.where(np.logical_and(freqs>=10, freqs<=12))
+                mu1 = psd1[mu_indices].mean()
+                mu2 = psd2[mu_indices].mean()
+                l, r = list(clf.predict_proba(np.array([mu1,mu2]).reshape(1,-1))[0])
+                prev_predictions.append(l)
+                l = np.array(prev_predictions).mean()
+                r = 1 - l
+                # clear buffers
+                prev_predictions = []
+                prev_states = []
+                if l > r:
+                    return 'L'
+                return 'R'
+            prev_predictions = []
+            prev_states = []
+            return 'BLINK'
+        if state == 'clear' and last_change * pred_frequency >= 1:  # we're at least 1 second away from when we stopped
+            if last_change * pred_frequency <= 2:                   # we're between 1 and 2 seconds away from when we stopped
+                psd1, freqs = mlab.psd(np.squeeze(ch[0,250:]),      # so we use a 1 second window
+                                   NFFT=250,
+                                   window=mlab.window_hanning,
+                                   Fs=250,
+                                   noverlap=0
+                                   ) 
+                psd2, freqs = mlab.psd(np.squeeze(ch[7,250:]),
+                                   NFFT=250,
+                                   window=mlab.window_hanning,
+                                   Fs=250,
+                                   noverlap=0
+                                   )
+            mu_indices = np.where(np.logical_and(freqs>=10, freqs<=12))
+            mu1 = psd1[mu_indices].mean()
+            mu2 = psd2[mu_indices].mean()
+            l, r = list(clf.predict_proba(np.array([mu1,mu2]).reshape(1,-1))[0])
+            if l > 0.8:     # only return if we are very confident; otherwise, delay decision
+                prev_predictions = []
+                prev_states = []
+                return 'L'
+            if r > 0.8:
+                prev_predictions = []
+                prev_states = []
+                return 'R'
+            prev_predictions.append(l)  # append prediction for voting
+            
+
+
+    #print("{:2.1f}, {:2.1f}".format(mean_psd, blink_psd/mean_psd))
 
 @sio.on('to ML (state)')
 def change_state(received_state):
 
-    global state
-    print(state);
-    state = received_state # forward, stop, turning, intermediate
+    global wheelchair_state
+    global last_change
+    if (received_state['state'] != wheelchair_state):
+        last_change = 0
+    wheelchair_state = received_state['state'] # forward, stop, turning, intermediate
 
 @sio.on('timeseries-prediction')
 def on_message(data):
@@ -91,8 +176,7 @@ def on_message(data):
         to_pop = len(buffer_data) - 500
         buffer_data = buffer_data[to_pop:]
         response = predict(buffer_data)
-    print('hi')
-    sio.emit('data from ML', {'response': 'L',
+    sio.emit('data from ML', {'response': response,
                               'left-prob': random.uniform(0, 1),
                               'right-prob': random.uniform(0, 1),
                               'blink-prob': -1})
