a3b46079f842404d427ca31e5df3c3936d9be40a
Roland <roland@riachi.com>
2019-04-12 14:55:59 -0400

    added comments

---
+++ offline/signal_processing/yoink.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  1 21:35:25 2019
+
+@author: roland
+"""
+
+'''
+TODO:
+    1) Get data
+    2) Resample
+    3) Format
+    
+    If data set has higher sample rate, take ratio (a:1, take every a'th pt)
+    If data set has lower sample rate, cry.
+'''
+
+import numpy as np
+#Import all the mne stuf later
+
+print("IN PROGRESS")
+
