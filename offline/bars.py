c7d5432dab77185e9794bef4b16d0ed31230a507
Marley <marleyxiong0@gmail.com>
2019-04-26 13:26:02 -0400

    code for generating/plotting error bars

--- offline/bars.py
+++ offline/bars.py
@@ -103,8 +103,56 @@ chan_2 = [[75.60521415270019,
           52.352941176470594,
           49.9264705882353,
           50.882352941176464]]
-chan_4 = np.array(chan_4).T
-chan_2 = np.array(chan_2).T
+chan_2 = [[[76.22794026903615, 3.5685147247787223],
+  [81.85703185703186, 4.538309820571024],
+  [87.29591836734694, 3.703346003674905]],
+ [[63.55855855855856, 4.98455531995419],
+  [69.01515151515152, 3.975649019212704],
+  [74.56666666666666, 5.070393366287168]],
+ [[66.15644631265788, 4.405181961106589],
+  [72.60848522191708, 6.255148913963715],
+  [76.86926569848113, 5.7705459168686914]],
+ [[57.72384629527487, 1.5627098117352618],
+  [60.18794818413139, 6.9625307750503165],
+  [62.86666666666666, 7.3805258636687405]],
+ [[51.09797297297297, 2.8826794251825953],
+  [52.55681818181819, 3.6286119239937324],
+  [54.775, 5.075615726195197]],
+ [[49.56819427288433, 3.3876808129159284],
+  [49.23754021872301, 3.959675319533922],
+  [49.53172606255181, 5.972517957341434]],
+ [[51.554054054054056, 3.6850660059522657],
+  [51.13636363636363, 6.125364253331906],
+  [52.175, 8.528884745381426]]]
+ 
+chan_4 = [[[78.28119215105517, 2.371995233658082],
+  [83.41103341103342, 2.4218754009145576],
+  [88.14285714285715, 3.5341298194113624]],
+ [[65.05630630630631, 5.225965406568135],
+  [69.69696969696969, 4.152178668498399],
+  [73.43333333333334, 3.7101961613310386]],
+ [[62.63813258500653, 3.360721360827933],
+  [66.10948005846106, 6.280873434432706],
+  [72.92946114892048, 8.482918245977306]],
+ [[59.14460378746092, 2.8140893954289794],
+  [60.193731205181585, 8.169242914015365],
+  [62.973737373737364, 9.057154731561441]],
+ [[50.50675675675676, 2.6927604291877243],
+  [53.63636363636364, 5.306546453363807],
+  [54.675000000000004, 5.581834375901884]],
+ [[49.54785986369437, 3.934440578681111],
+  [49.11768158810407, 4.625096371438731],
+  [49.496935818297345, 5.634202338946582]],
+ [[51.38513513513514, 2.84927713719267],
+  [51.81818181818182, 6.619799100317042],
+  [51.275, 9.611288935413398]]]
+#chan_4 = np.array(chan_4).T
+
+chan_4_err = np.array(chan_4)[:,:,1].T
+chan_4 = np.array(chan_4)[:,:,0].T
+chan_2_err = np.array(chan_2)[:,:,1].T
+chan_2 = np.array(chan_2)[:,:,0].T
+
 window_sizes = [1,2,4,6,8]
 
 limit = 3
@@ -132,9 +180,9 @@ if both:
 """
 i = 0
 two_channels = []
-for window_, window_s in zip(chan_2, window_sizes):
+for window_, window_s,err_s in zip(chan_2, window_sizes,chan_2_err):
     r = [x + barWidth * i for x in r1]
-    two_channels.append(plt.bar(r, window_, width=barWidth, color=colors[int(i/2)],edgecolor='white', label=str(window_s) + ' s', alpha=0.5))
+    two_channels.append(plt.bar(r, window_, width=barWidth, yerr=err_s, error_kw=dict(lw=1, capsize=1, capthick=1), color=colors[int(i/2)],edgecolor='white', label=str(window_s) + ' s', alpha=0.5))
     i+= 1
     if both:
         i += 1
@@ -144,9 +192,9 @@ for window_, window_s in zip(chan_2, window_sizes):
 """
 i = 1
 four_channels = []
-for window_, window_s in zip(chan_4, window_sizes):
+for window_, window_s,err_s in zip(chan_4, window_sizes,chan_4_err):
     r = [x + barWidth * i for x in r1]
-    four_channels.append(plt.bar(r, window_, width=barWidth, color=colors[int(i/2)], edgecolor='white', label=str(window_s) + ' s'))
+    four_channels.append(plt.bar(r, window_, width=barWidth, yerr=err_s, error_kw=dict(lw=1, capsize=1, capthick=1), color=colors[int(i/2)], edgecolor='white', label=str(window_s) + ' s'))
     i += 1
     if both:
         i+= 1
@@ -155,8 +203,8 @@ for window_, window_s in zip(chan_4, window_sizes):
 #plt.title('Inter-subject Accuracy by Window Size',fontweight='bold')
 # plt.style.use('ggplot')
 plt.ylabel('Accuracy (%)')#, fontweight='bold')
-plt.xlabel('Subject')#, fontweight='bold')
-plt.xticks([r + barWidth*distinct_windows for r in r1], [i for i in range(1,num_subjects + 1)])
+#plt.xlabel('Subject')#, fontweight='bold')
+plt.xticks([r + barWidth*distinct_windows for r in r1], ["S" + str(i) for i in range(1,num_subjects + 1)])
 
 # Create legend & Show graphic
 legend1 = plt.legend(handles=four_channels, bbox_to_anchor=(1.04,0.65), title="4 channels", loc="upper left")
